cb2fcd762a026e2d8a15e4ebc6e67b34427667e0
Davood Wadi <62418145+davoodwadi@users.noreply.github.com>
2024-08-11 14:04:48 +0330

    commented out netlify function

--- netlify/functions/getCompletions.js
+++ netlify/functions/getCompletions.js
@@ -1,24 +1,24 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-exports.handler = async (event, context) => {
-  try {
-    // Make a POST request to the Render server
-    await axios.post('https://jschatapi.onrender.com/api/gpt/completions/stream', {}, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'touch': 'yes',
-      }
-    });
+// exports.handler = async (event, context) => {
+//   try {
+//     // Make a POST request to the Render server
+//     await axios.post('https://jschatapi.onrender.com/api/gpt/completions/stream', {}, {
+//       headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded',
+//         'touch': 'yes',
+//       }
+//     });
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'touch success. data received from server using function.' }),
-    };
-  } catch (error) {
-    console.error('Error:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Error in sending data to the server using function' }),
-    };
-  }
-};
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ message: 'touch success. data received from server using function.' }),
+//     };
+//   } catch (error) {
+//     console.error('Error:', error);
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'Error in sending data to the server using function' }),
+//     };
+//   }
+// };
